bcd7f719200dddc647d4b11a98832356d164f3d1
Pedro Javier Paneca Cordova <pjpc@ic.ufal.br>
2020-01-28 20:27:40 -0300

    rocketshoes finalizado

---
+++ rocketshoes_mobile/src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Wrapper, Container, Logo, BasketContainer, ItemCount } from './styles';
+
+function Header({ navigation }) {
+  const cartSize = useSelector(state => state.cart.length);
+
+  return (
+    <Wrapper>
+      <Container>
+        <BasketContainer onPress={() => navigation.navigate('Main')}>
+          <Logo />
+        </BasketContainer>
+        <BasketContainer onPress={() => navigation.navigate('Cart')}>
+          <Icon name="shopping-basket" color="#FFF" size={24} />
+          <ItemCount>{cartSize || 0}</ItemCount>
+        </BasketContainer>
+      </Container>
+    </Wrapper>
+  );
+}
+
+export default Header;
